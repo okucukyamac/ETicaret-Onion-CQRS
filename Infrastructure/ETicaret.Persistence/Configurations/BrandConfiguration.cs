4f94565d579fc65426371021903914b19c6fa80a
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2024-01-15 22:57:30 +0300

    entity configuration

---
+++ Infrastructure/ETicaret.Persistence/Configurations/BrandConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Bogus;
+using ETicaret.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ETicaret.Persistence.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(a=>a.Name).HasMaxLength(256);
+
+            Faker faker = new Faker("tr");
+
+            Brand brand1 = new()
+            {
+                Id = 1,
+                Name = faker.Commerce.Department(),
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+            Brand brand2 = new()
+            {
+                Id = 2,
+                Name = faker.Commerce.Department(),
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+            Brand brand3 = new()
+            {
+                Id = 3,
+                Name = faker.Commerce.Department(),
+                CreatedDate = DateTime.Now,
+                IsDeleted = true
+            };
+
+            builder.HasData(brand1,brand2,brand3);
+        }
+    }
+}
---
+++ Infrastructure/ETicaret.Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Bogus;
+using ETicaret.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            Faker faker = new Faker("tr");
+
+            Product product1 = new Product()
+            {
+                Id = 1,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BrandId = 1,
+                Discount = faker.Random.Decimal(0, 10),
+                Price = faker.Finance.Amount(10, 1000),
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+            Product product2 = new Product()
+            {
+                Id = 2,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BrandId = 3,
+                Discount = faker.Random.Decimal(0, 10),
+                Price = faker.Finance.Amount(10, 1000),
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+            builder.HasData(product1, product2);
+        }
+    }
+}
