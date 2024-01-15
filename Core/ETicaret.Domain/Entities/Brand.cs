d5a883f9b9a4f423834636789e84fde886b16733
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2024-01-15 22:20:56 +0300

    temel entityler oluştruldu

---
+++ Core/ETicaret.Domain/Entities/Brand.cs
@@ -0,0 +1,21 @@
+﻿using ETicaret.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Domain.Entities
+{
+    public class Brand:EntityBase
+    {
+        public Brand()
+        {
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
